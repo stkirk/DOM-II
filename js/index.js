5e25c562af731691132692689eb84ae66dc9b719
stkirk <shaanekirk@gmail.com>
2021-06-04 13:40:49 -0400

    added click event listener to all 3 sign me up! buttons

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+//Sign Me Up! button click
+const signMeUp = document.querySelectorAll(".destination .btn");
+console.log("signMeUp", signMeUp);
+signMeUp.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    //do something when button is clicked
+    console.log("Someone clicked me!");
+  });
+});
