72c639112024dafca8bdd123eaa79df6694b5f71
stkirk <shaanekirk@gmail.com>
2021-06-05 12:13:33 -0400

    added keydown alert on entire document

--- js/index.js
+++ js/index.js
@@ -4,18 +4,26 @@
 const signMeUp = document.querySelectorAll(".destination .btn");
 console.log("signMeUp", signMeUp);
 signMeUp.forEach((element) => {
-  element.addEventListener("click", (event) => {
+  element.addEventListener("click", (e) => {
     //do something when button is clicked
     console.log("Someone clicked me!");
   });
 });
 
+//Highlight sections on mouseover and un-highlight on mouseout
 const section = document.querySelectorAll("section");
 section.forEach((element) => {
-  element.addEventListener("mouseover", () => {
+  element.addEventListener("mouseover", (e) => {
     element.style.backgroundColor = "#FFF0F5";
   });
-  element.addEventListener("mouseout", () => {
+  element.addEventListener("mouseout", (e) => {
     element.style.backgroundColor = "";
   });
 });
+
+//Text alert for keydown
+document.addEventListener("keydown", (e) => {
+  alert(`ALERT! you have pressed the ${e.key} key!!!!!!!`);
+});
+
+//Wheel makes fonts bigger
